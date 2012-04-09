6151453952036e1b0ce0ae6c66de8c331411b398
Frontino Jr <frontinojr@gmail.com>
2012-04-09 15:27:25 -0300

    duo commito

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vini::Application.config.secret_token = 'e1bd5a56b27cb3764a2bc3fc32bb6fa07e34065fef6538fc843dcd8c14c8227cfa93276e4ef83ee80d8427b46c45296a40c12ad42127341a05bb6ba2d6922b5c'
